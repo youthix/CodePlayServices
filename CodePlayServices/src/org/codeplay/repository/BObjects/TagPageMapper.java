ae2c5717a7a992801ce3eaa3f84a4d46d592214d
Saurabh Dixit <saurabh.iips@gmail.com>
2016-04-11 23:03:50 +0530

    Added

---
+++ CodePlayServices/src/org/codeplay/repository/BObjects/TagPageMapper.java
@@ -0,0 +1,21 @@
+package org.codeplay.repository.BObjects;
+
+public class TagPageMapper {
+	
+	public String tags;
+	public String pageIds;
+	
+	public String getTags() {
+		return tags;
+	}
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	public String getPageIds() {
+		return pageIds;
+	}
+	public void setPageIds(String pageIds) {
+		this.pageIds = pageIds;
+	}
+	
+}
