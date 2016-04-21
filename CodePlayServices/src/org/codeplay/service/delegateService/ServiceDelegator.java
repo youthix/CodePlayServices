3cbce9af28b7811c697b9bc3cc67130c6573f3dd
Saurabh Dixit <saurabh.iips@gmail.com>
2016-04-21 22:55:11 +0530

    updated

---
+++ CodePlayServices/src/org/codeplay/service/delegateService/ServiceDelegator.java
@@ -0,0 +1,21 @@
+package org.codeplay.service.delegateService;
+
+import org.codeplay.presentation.entities.UserList;
+import org.codeplay.repository.RepositoryDelegate.RepositoryDelegator;
+
+public class ServiceDelegator {
+	
+	RepositoryDelegator repositryDelegator;
+	
+	public String fetchPages(String tags) {
+		
+		return null;
+	}
+
+	
+	public UserList fetchUsers(String pageIds) {
+		
+		return null;
+	}
+
+}
