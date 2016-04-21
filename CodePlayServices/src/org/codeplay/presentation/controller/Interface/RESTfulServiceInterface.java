ac1de598beaf3c2e076ec098b02dd9731410c3b3
Saurabh Dixit <saurabh.iips@gmail.com>
2016-04-21 22:56:41 +0530

    Updated

--- CodePlayServices/src/org/codeplay/presentation/controller/Interface/RESTfulServiceInterface.java
+++ CodePlayServices/src/org/codeplay/presentation/controller/Interface/RESTfulServiceInterface.java
@@ -1,5 +1,26 @@
 package org.codeplay.presentation.controller.Interface;
 
-public class RESTfulServiceInterface {
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import org.codeplay.presentation.entities.UserList;
+
+@Path("/hotornot")
+public interface RESTfulServiceInterface {
+
+	@POST
+	@Path("/pages")
+	@Produces(MediaType.APPLICATION_JSON)
+	String fetchPages(String tags);
+	
+	@POST
+	@Path("/users")
+	@Produces(MediaType.APPLICATION_JSON)
+	UserList fetchUsers(String pageIds);
+	
+	@GET
+	String helloWorld();
 }
