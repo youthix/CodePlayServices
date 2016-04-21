ea695b5413fe157879bc111dbde7c81b69b5939b
Saurabh Dixit <saurabh.iips@gmail.com>
2016-04-21 22:56:18 +0530

    updated

--- CodePlayServices/src/org/codeplay/presentation/controller/Impl/RESTfulServiceImpl.java
+++ CodePlayServices/src/org/codeplay/presentation/controller/Impl/RESTfulServiceImpl.java
@@ -1,5 +1,27 @@
 package org.codeplay.presentation.controller.Impl;
 
-public class RESTfulServiceImpl {
+import org.codeplay.presentation.controller.Interface.RESTfulServiceInterface;
+import org.codeplay.presentation.entities.UserList;
+import org.codeplay.service.delegateService.ServiceDelegator;
+
+public class RESTfulServiceImpl implements RESTfulServiceInterface{
+
+	ServiceDelegator serviceDelegator;
+	@Override
+	public String fetchPages(String tags) {		
+		String pages=serviceDelegator.fetchPages(tags);
+		return pages;
+	}
+
+	@Override
+	public UserList fetchUsers(String pageIds) {
+		UserList list=serviceDelegator.fetchUsers(pageIds);
+		return list;
+	}
+
+	@Override
+	public String helloWorld() {		
+		return "Welcome to Code Play Services !!";
+	}
 
 }
