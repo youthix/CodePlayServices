608b089613530fdeb92f3b753402355885f1deaf
Saurabh Dixit <saurabh.iips@gmail.com>
2016-04-21 22:11:02 +0530

    Updated

--- CodePlayServices/src/org/codeplay/repository/Mapper/TagsPageIDMapper.java
+++ CodePlayServices/src/org/codeplay/repository/Mapper/TagsPageIDMapper.java
@@ -7,12 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.codeplay.repository.BObjects.TagPage;
+import org.codeplay.repository.BObjects.User;
+import org.springframework.jdbc.core.RowMapper;
 
 /**
  * @author surabh
  *
  */
-public class TagsPageIDMapper {
+public class TagsPageIDMapper implements RowMapper<TagPage>  {
 	public TagPage mapRow(ResultSet rs, int rowNum) throws SQLException {
 		TagPage tagPageObj = new TagPage();
 
