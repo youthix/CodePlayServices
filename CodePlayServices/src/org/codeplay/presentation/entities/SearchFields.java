de7b6d7707b001248f6bb32911034393f3f69d9a
saurabh agarwal <inbox.saurabh@gmail.com>
2016-04-23 21:02:25 +0530

    updated

---
+++ CodePlayServices/src/org/codeplay/presentation/entities/SearchFields.java
@@ -0,0 +1,79 @@
+package org.codeplay.presentation.entities;
+
+public class SearchFields {
+	
+	private String ageGroup;
+	
+	private String gender;
+	
+	private String lives_in_country;
+	
+	private String live_in_id;
+	
+	private String currently_at_id;
+	
+	private String pageID;
+	
+	private String tag;
+
+	public String getAgeGroup() {
+		return ageGroup;
+	}
+
+	public void setAgeGroup(String ageGroup) {
+		this.ageGroup = ageGroup;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getLives_in_country() {
+		return lives_in_country;
+	}
+
+	public void setLives_in_country(String lives_in_country) {
+		this.lives_in_country = lives_in_country;
+	}
+
+	public String getLive_in_id() {
+		return live_in_id;
+	}
+
+	public void setLive_in_id(String live_in_id) {
+		this.live_in_id = live_in_id;
+	}
+
+	public String getCurrently_at_id() {
+		return currently_at_id;
+	}
+
+	public void setCurrently_at_id(String currently_at_id) {
+		this.currently_at_id = currently_at_id;
+	}
+
+	public String getPageID() {
+		return pageID;
+	}
+
+	public void setPageID(String pageID) {
+		this.pageID = pageID;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	
+	
+	
+	
+
+}
