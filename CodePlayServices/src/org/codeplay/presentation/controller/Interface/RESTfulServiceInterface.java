49e4dbcff32dc192d3d38c8c1667ff0dc41fdad2
Saurabh Dixit <saurabh.iips@gmail.com>
2016-04-23 12:44:36 +0530

    Updated

--- CodePlayServices/src/org/codeplay/presentation/controller/Interface/RESTfulServiceInterface.java
+++ CodePlayServices/src/org/codeplay/presentation/controller/Interface/RESTfulServiceInterface.java
@@ -1,13 +1,6 @@
 package org.codeplay.presentation.controller.Interface;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.codeplay.presentation.entities.UserList;
-
 
 public interface RESTfulServiceInterface {
 
