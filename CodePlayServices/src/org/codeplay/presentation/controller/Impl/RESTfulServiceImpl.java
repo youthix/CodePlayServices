d3e6ad19af780b0c2958379f543d2c8c8a87a202
saurabh agarwal <inbox.saurabh@gmail.com>
2016-04-24 10:07:43 +0530

    updated

--- CodePlayServices/src/org/codeplay/presentation/controller/Impl/RESTfulServiceImpl.java
+++ CodePlayServices/src/org/codeplay/presentation/controller/Impl/RESTfulServiceImpl.java
@@ -1,5 +1,9 @@
 package org.codeplay.presentation.controller.Impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,10 +12,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.codeplay.presentation.controller.Interface.RESTfulServiceInterface;
+import org.codeplay.presentation.entities.RequestObj;
+import org.codeplay.presentation.entities.ResponseObj;
+import org.codeplay.presentation.entities.SearchFields;
 import org.codeplay.presentation.entities.UserList;
+import org.codeplay.repository.BObjects.User;
 import org.codeplay.service.delegateService.ServiceDelegator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 
 @Path("/hotornot")
 public class RESTfulServiceImpl implements RESTfulServiceInterface{
@@ -25,6 +31,56 @@ public class RESTfulServiceImpl implements RESTfulServiceInterface{
 	public String fetchPages(@FormParam("tags") String tags) {			
 		String pages=serviceDelegator.fetchPages(tags,"1519","female");
 		return pages;
+	}
+	
+	
+	@POST
+	@Path("/pageIds")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ResponseObj fetchPageIds(RequestObj requestParam) {
+		
+		System.out.println(requestParam.getSearchFieldsList().size());
+		
+		List <SearchFields> searchFieldsList = requestParam.getSearchFieldsList();
+		
+		for (SearchFields searchFieldsObj : searchFieldsList){
+			
+			System.out.println(searchFieldsObj.getTag());
+		}
+		
+		ResponseObj responseObj = new ResponseObj();
+		
+		List<UserList> listOfuserList = new ArrayList<UserList>();
+		
+		for (int i=1; i<5;i++){
+			
+			UserList userListObj = new UserList();
+			
+			userListObj.setPageID("1");
+			userListObj.setTag("Tag");
+			List<User> users = new ArrayList<User>();
+			for(int j=1; j<5;j++){
+				User userObj= new User();
+				userObj.setAge("15");
+				userObj.setDateOfBirth("1234");
+				users.add(userObj);
+			}
+
+			userListObj.setUserList(users);
+			
+			listOfuserList.add(userListObj);
+		}
+		
+		
+		
+		
+		responseObj.setUserList(listOfuserList);
+		
+		
+		//String pages=serviceDelegator.fetchPages(tags,"1519","female");
+		//return null;
+		return responseObj;
 	}
 
 	@Override
